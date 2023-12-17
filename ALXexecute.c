5ac29d2e41005465781c1ec5104db9445fc1adcf
Benjamin <benjminrichie@gmail.com>
2023-12-17 13:08:36 -0800

    ALXexecute.c

--- ALXexecute.c
+++ ALXexecute.c
@@ -35,22 +35,22 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 				{"stack", f_stack},
 				{NULL, NULL}
 				};
-	unsigned int eye = 0;
+	unsigned int i = 0;
 	char *op;
 
 	op = strtok(content, " \n\t");
 	if (op && op[0] == '#')
 		return (0);
 	bus.arg = strtok(NULL, " \n\t");
-	while (opst[eye].opcode && op)
+	while (opst[i].opcode && op)
 	{
-		if (strcmp(op, opst[eye].opcode) == 0)
-		{	opst[eye].f(stack, counter);
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(stack, counter);
 			return (0);
 		}
-		eye++;
+		i++;
 	}
-	if (op && opst[eye].opcode == NULL)
+	if (op && opst[i].opcode == NULL)
 	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
 		fclose(file);
 		free(content);
