2521ac9ae4707bcd78ccd76eb23ac76e8bde4251
Benjamin <benjminrichie@gmail.com>
2023-12-18 03:41:06 -0800

    After second deadline

--- subOpreytor.c
+++ subOpreytor.c
@@ -1,31 +1,35 @@
 #include "monty.h"
 
 /**
- * f_sub - Subtracts the top element of the stack from the second top element.
- * @head: Pointer to the head of the stack.
- * @counter: Line number in the Monty bytecode file.
+ * f_sub - This func will subtract
  *
- * Return: No return value.
+ * @head: This is the stack head
+ *
+ * @counter: This is the line_number
+ *
+ * Return: no return
+ *
  */
+
 void f_sub(stack_t **head, unsigned int counter)
 {
-    stack_t *aux;
-    int sus, nodes;
+        stack_t *aux;
+        int sus, nodes;
 
-    aux = *head;
-    for (nodes = 0; aux != NULL; nodes++)
-        aux = aux->next;
-    if (nodes < 2)
-    {
-        fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
-    aux = *head;
-    sus = aux->next->n - aux->n;
-    aux->next->n = sus;
-    *head = aux->next;
-    free(aux);
+        aux = *head;
+        for (nodes = 0; aux != NULL; nodes++)
+                aux = aux->next;
+        if (nodes < 2)
+        {
+                fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
+                fclose(bus.file);
+                free(bus.content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
+        }
+        aux = *head;
+        sus = aux->next->n - aux->n;
+        aux->next->n = sus;
+        *head = aux->next;
+        free(aux);
 }
